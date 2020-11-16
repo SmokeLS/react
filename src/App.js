56c2ffee57d0d7e8ac04269a11468e69c90b406f
Andrey <smokels1998@gmail.com>
2020-11-16 13:26:43 +0300

    props task https://daruse.ru/zadaniya-po-react-dlya-prokachki

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
+import Header from './components/header';
+import Navigation from './components/nav';
+import Footer from './components/footer';
+
+import Button from './components/content/buttons';
+import Card from './components/content/card';
+import Breadcrumb from './components/content/breadcrumb';
+import Alert from './components/content/alerts';
+import Input from "./components/content/input";
+import Title from "./components/content/title";
+
 import './App.css';
+
+import {ReactComponent as Globe} from "./images/globe.svg";
+
+const list = [
+  {title: 'Home', link: '/home', id: 1},
+  {title: 'Library', link: '/library', id: 2},
+  {title: 'Data', link: '/data', id: 3},
+  {title: 'News', link: '/news', id: 4},
+  {title: 'About us', link: '/aboutus', id: 5}
+];
+
+const copyright = "@copyright by me";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header title="MySite" Logo={Globe}/>
+      <Navigation menu={list}/>
+      <Footer copyright={copyright}/>
+
+      <Alert text="some text" color="alert-success"/>
+      <Button text="some text1" style="btn-primary" type="button"/>
+      <Button text="some text2" style="btn-success btn-lg" type="a" href="#"/>
+      <Button text="some text3" style="btn-outline-warning" type="submit"/>
+      <Card href="https://zagge.ru/wp-content/uploads/2019/06/aHR0cDovL3d3dy5saXZlc2N.jpg" text="<p>text text</p>"/>
+      <Breadcrumb items={list}/>
+
+      <Title text="title text" size="44" color="red"/>
+      <Input type="text" placeholder="placeholder" value="value" /> 
+    </>
   );
 }
 
